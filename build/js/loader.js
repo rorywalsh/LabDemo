fffcd56462cf930e04f83786f133fa07ca550f16
Rory Walsh <rorywalsh@ear.ie>
2021-05-28 15:02:30 +0100

    Update loader.js

--- build/js/loader.js
+++ build/js/loader.js
@@ -90,7 +90,7 @@ var loadScene = function (name, incremental, sceneLocation, then) {
     engine.resize();
 
     var dlCount = 0;
-    BABYLON.SceneLoader.Load("https://raw.githubusercontent.com/rorywalsh/LabDemo/master/Scenes/Heart/Text.babylon", engine, function (newScene) {
+    BABYLON.SceneLoader.Load("https://raw.githubusercontent.com/rorywalsh/LabDemo/master/Scenes/Heart/Heart.babylon", engine, function (newScene) {
         scene = newScene;
         scene.executeWhenReady(function () {
             canvas.style.opacity = 1;
