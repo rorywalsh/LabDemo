d9c88f7c3355677d5d6a9c171f78e7ffdbe7aa87
Rory <rwalsh@Rorys-Mac-mini.fritz.box>
2024-04-08 16:52:08 +0100

    updating audio classes, and cleaned up code

--- audioClasses.js
+++ audioClasses.js
@@ -1,9 +1,27 @@
+/* One shot collision sound class that creates an optional visible mesh for testing. 
+
+OneShotCollisionSound({file:'string', x: number, z: number, w: number, visible: true/false, color: 'string', volume:number, timeBetweenPlays:number})
+
+'file' can also be passed an array of files. These would then be played in random order. 
+
+For example:
+
+let triggerSounds = [];
+let files = [];
+files.push("./build/assets/2.wav");
+files.push("./build/assets/3.wav");
+files.push("./build/assets/4.wav");
+triggerSounds.push(new OneShotCollisionSound({ file: files, x: 2, z: 0.5, visible: true }));
+
+*/
+
 class OneShotCollisionSound {
     constructor(args) {
         //Simple crate
         this.y = (typeof args.y === 'undefined' ? 1 : args.y);
         this.w = (typeof args.w === 'undefined' ? .3 : args.w);
-        this.h = (typeof args.h === 'undefined' ? .3 : args.h);
+        this.h = (typeof args.h === 'undefined' ? .1 : args.h);
+        this.color = (typeof args.color === 'undefined' ? 'white' : args.color);
         this.visible = (typeof args.visible === 'undefined' ? true : args.visible);
         this.name = (typeof args.name === 'undefined' ? args.file[0] : args.name);
         this.volume = (typeof args.volume === 'undefined' ? 0.5 : args.volume);
@@ -12,6 +30,10 @@ class OneShotCollisionSound {
         this.position = new BABYLON.Vector3(args.x, this.y, args.z);
         this.box = BABYLON.Mesh.CreateBox(this.name, 3, scene);
         this.box.material = new BABYLON.StandardMaterial("Mat", scene);
+        const [, r, g, b, a] = colorKeywordToRGB(this.color).replace(/\s/g, "").match(/rgba?\((\d+(?:\.\d+)?),(\d+(?:\.\d+)?),(\d+(?:\.\d+)?)(?:,(\d+(?:\.\d+)?))?\)/i);
+        this.box.material.emissiveColor = new BABYLON.Color3(r, g, b, .5);
+        this.box.material.alpha = 0.1;
+        this.box.material.zOffset = 1
         if (this.visible)
             this.box.material.wireframe = true;
         else
@@ -22,17 +44,17 @@ class OneShotCollisionSound {
         // Create and load the sound async
         this.sound = null;
         console.log(args.file);
-        if(Array.isArray(args.file)){
-            args.file.forEach( sound => {
-                this.sounds.push(new BABYLON.Sound(sound, sound, scene, function() {
+        if (Array.isArray(args.file)) {
+            args.file.forEach(sound => {
+                this.sounds.push(new BABYLON.Sound(sound, sound, scene, function () {
                     // Call with the sound is ready to be played (loaded & decoded)
                     // TODO: add your logic
                     console.log(sound);
                 }, { loop: false, autoplay: false, spatialSound: false, volume: this.volume }));
             })
         }
-        else{
-            this.sound = new BABYLON.Sound(this.name, args.file[0], scene, function() {
+        else {
+            this.sound = new BABYLON.Sound(this.name, args.file, scene, function () {
                 // Call with the sound is ready to be played (loaded & decoded)
                 // TODO: add your logic
                 console.log("Sound ready to be played!");
@@ -47,12 +69,12 @@ class OneShotCollisionSound {
     play() {
         var that = this;
         if (this.canPlay === true) {
-            if(this.sounds.length > 0){
+            if (this.sounds.length > 0) {
                 let index = Math.floor(Math.random() * this.sounds.length);
                 this.sounds[index].play();
             }
-            else{
-                if(this.sound){
+            else {
+                if (this.sound) {
                     this.sound.play();
                 }
             }
@@ -68,15 +90,19 @@ class AreaSound {
         this.y = (typeof args.y === 'undefined' ? 1 : args.y);
         this.w = (typeof args.w === 'undefined' ? .3 : args.w);
         this.h = (typeof args.h === 'undefined' ? .3 : args.h);
+        this.color = (typeof args.color === 'undefined' ? 'white' : args.color);
         this.visible = (typeof args.visible === 'undefined' ? true : args.visible);
         this.name = (typeof args.name === 'undefined' ? args.file : args.name);
         this.rolloff = (typeof args.rolloff === 'undefined' ? 5 : args.rolloff);
         this.volume = (typeof args.volume === 'undefined' ? 1 : args.volume);
 
         this.position = new BABYLON.Vector3(args.x, this.y, args.z);
-        let size = (5/this.rolloff)*3;
-        this.sphere = BABYLON.MeshBuilder.CreateSphere("sphere", {diameter:1, diameterY:size, diameterX:size, diameterZ:size}, scene); //scene is optional and defaults to the current scene
+        let size = (8 / this.rolloff) * 3;
+        this.sphere = BABYLON.MeshBuilder.CreateSphere("sphere", { diameter: 1, diameterY: size, diameterX: size, diameterZ: size }, scene); //scene is optional and defaults to the current scene
         this.sphere.material = new BABYLON.StandardMaterial("Mat", scene);
+        const [, r, g, b, a] = colorKeywordToRGB(this.color).replace(/\s/g, "").match(/rgba?\((\d+(?:\.\d+)?),(\d+(?:\.\d+)?),(\d+(?:\.\d+)?)(?:,(\d+(?:\.\d+)?))?\)/i);
+        this.sphere.material.emissiveColor = new BABYLON.Color3(r, g, b, .5);
+        this.sphere.material.alpha = 0.1;
         if (this.visible)
             this.sphere.material.wireframe = true;
         else
@@ -85,10 +111,25 @@ class AreaSound {
         //this.sphere.scaling = new BABYLON.Vector3(this.w, 1, this.h);
         this.sphere.position = new BABYLON.Vector3(args.x, this.y, args.z);
         // Create and load the sound async
-        this.sound = new BABYLON.Sound(this.name, args.file, scene, function() {
+        this.sound = new BABYLON.Sound(this.name, args.file, scene, function () {
             console.log(args.file + " is ready to be played!");
-        }, { loop: true, autoplay: true, spatialSound: true, distanceModel:"exponential", volume: this.volume, rolloffFactor:this.rolloff });
+        }, { loop: true, autoplay: true, spatialSound: true, distanceModel: "exponential", volume: this.volume, rolloffFactor: this.rolloff });
         // Sound will now follow the mesh position
         this.sound.attachToMesh(this.sphere);
     }
 };
+
+//https://stackoverflow.com/a/72808544
+const colorKeywordToRGB = (colorKeyword) => {
+    // CREATE TEMPORARY ELEMENT
+    let el = document.createElement('div');
+    // APPLY COLOR TO TEMPORARY ELEMENT
+    el.style.color = colorKeyword;
+    // APPEND TEMPORARY ELEMENT
+    document.body.appendChild(el);
+    // RESOLVE COLOR AS RGB() VALUE
+    let rgbValue = window.getComputedStyle(el).color;
+    // REMOVE TEMPORARY ELEMENT
+    document.body.removeChild(el);
+    return rgbValue;
+}
--- demo.js
+++ demo.js
@@ -5,7 +5,7 @@
     doNotUseCDN: false,
     collisions: true,
     offline: true,
-    onload: function() {
+    onload: function () {
         scene.getMeshByName("Labels").setEnabled(false);
         scene.getMeshByName("lums").useVertexColors = false;
         scene.gravity.scaleInPlace(0.5);
@@ -14,11 +14,12 @@
         scene.gravity.scaleInPlace(0.5);
         let camera = scene.activeCamera;
         camera.checkCollisions = true;
+
+        //add a box to the camera. We use this to check for collisions
         var cameraBox = BABYLON.Mesh.CreateBox("CameraBox", .1, scene);
         cameraBox.position = new BABYLON.Vector3(0, 0, 0);
         cameraBox.parent = camera;
         camera.ellipsoid = new BABYLON.Vector3(.4, .8, .4);
-
         cameraBox.isPickable = false;
         camera.speed = 0.1;
         scene.audioPositioningRefreshRate = 100;
@@ -27,52 +28,43 @@
         scene.fogDensity = 0.1;
         scene.fogColor = scene.clearColor;
 
-        let triggerSounds = [];
-        triggerSounds.push(new OneShotCollisionSound({ file: "./build/assets/2.wav", x: 2, z: 0.5, visible:true }));
-        triggerSounds.push(new OneShotCollisionSound({ file: "./build/assets/3.wav", x: 4.8, z: -3, visible:true }));
-        triggerSounds.push(new OneShotCollisionSound({ file: "./build/assets/4.wav", x: -5, z: -1, visible:true }));
-
-
-
+        //simple footsteps loop - set to play from the start. The volume is turned up and down as the player moves
         var footstepSounds = new BABYLON.Sound("Footsteps1", "./build/assets/footsteps.ogg", scene, null, {
             loop: true,
             autoplay: true,
-            volume: 1
+            volume: 0
         });
 
+        //all one-shot trigger sounds should be added to this array...
+        let triggerBoxes = [];
 
+        //creates a new sound collision box. x and z set its position, w its size. Set visible to false
+        //to remove the mesh from the scene. See audioClasses.js for more options.
+        triggerBoxes.push(new OneShotCollisionSound({ file: "./build/assets/3.wav", x: 2, z: 0.5, w: .2, visible: true, color: 'cyan' }));
 
-        let isWalking = false;
-        let prevIndex = -1;
-
-
-        // Load the sound and play it automatically once ready
-        var bubbles = new BABYLON.Sound("Bubbling", "./build/assets/bubbling.ogg", scene, null, {
-            loop: true,
-            autoplay: true,
-            volume: .5,
-            spatialSound: true,
-            distanceModel: "exponential",
-            rolloffFactor: 4
-        });
-
-        bubbles.setPosition(new BABYLON.Vector3(-1.16, 1, 1.74));
+        //create a new area sound. x: and z: set its position, rolloff sets its size. Set visible to false
+        //to remove the mesh from the scene. See audioClasses.js for more options.
+        let bubbles = new AreaSound({ file: "./build/assets/bubbling.ogg", x: -1.49, z: -0.41, visible: true, color: 'yellow', rolloff: 8, volume: 1 });
 
         scene.onKeyboardObservable.add((kbInfo) => {
             if (kbInfo.type === BABYLON.KeyboardEventTypes.KEYDOWN) {
-                if (kbInfo.event.key === 'ArrowUp' || kbInfo.event.keyCode == 87) {
-                    if (isWalking === false)
-                    footstepSounds.setVolume(.5);
-                    isWalking = true;
+                if (kbInfo.event.key === 'ArrowUp' || kbInfo.event.keyCode == 87 ||
+                    kbInfo.event.key === 'ArrowDown' || kbInfo.event.keyCode == 83 ||
+                    kbInfo.event.key === 'ArrowLeft' || kbInfo.event.keyCode == 65 ||
+                    kbInfo.event.key === 'ArrowRight' || kbInfo.event.keyCode == 68) {
+                        //every time the user moves, turn up volume of footsteps sample
+                        footstepSounds.setVolume(.5, .1);
+                }
+                else if (kbInfo.event.keyCode == 81) {
+                    // user just pressed q 
                 }
             } else if (kbInfo.type === BABYLON.KeyboardEventTypes.KEYUP) {
-                console.log(kbInfo.event.key);
-                if (kbInfo.event.key === 'ArrowUp' || kbInfo.event.keyCode == 87) {
-                    footstepSounds.setVolume(0);
-                    isWalking = false;
-                }
+                //every time the user stops moving, turn down volume of footsteps sample
+                footstepSounds.setVolume(0, .1);
             }
         });
+
+
 
         // GUI
         let GUIInfo = "Player's position";
@@ -81,31 +73,24 @@
         stackPanel.verticalAlignment = BABYLON.GUI.Control.VERTICAL_ALIGNMENT_TOP;
         stackPanel.isVertical = true;
         advancedTexture.addControl(stackPanel);
-
         var textInfo = new BABYLON.GUI.TextBlock();
         textInfo.color = "white";
         textInfo.fontSize = 16;
         textInfo.height = "30px";
         stackPanel.addControl(textInfo);
 
+        //this method gets call eah time a frame is rendered
         scene.onBeforeRenderObservable.add(() => {
             textInfo.text = "Player's position X:" + (camera.position.x).toFixed(2) + " Z:" + (camera.position.z).toFixed(2);
-
+            //check if the camera box collides with any of the trigger boxes - if so, play the corresponding sounds
             if (cameraBox) {
-                triggerSounds.forEach(oneShot => {
+                triggerBoxes.forEach(oneShot => {
                     if (cameraBox.intersectsMesh(oneShot.box, false)) {
                         oneShot.play();
                     } else
                         oneShot.canPlay = true;
                 });
             }
-
-
-
-
-
         });
-
     }
-
 };
